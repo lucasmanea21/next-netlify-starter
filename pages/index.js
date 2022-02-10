ce462e71a4eb1c6a52c83874f5b9e8e57adef606
lucasmanea21 <lucasmanea@Yahoo.com>
2022-02-10 09:41:48 +0200

    'update'

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
 import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { FaTelegram } from "react-icons/fa";
+import { Button } from "@mui/material";
 
+const StyledButton = ({ text, icon }) => (
+  <Button
+    style={{
+      backgroundColor: "#312c55",
+      color: "#fff",
+      textTransform: "none",
+      margin: 10,
+    }}
+  >
+    {icon}
+    <p style={{ margin: 0, marginLeft: 10, marginRight: 5 }}>{text}</p>
+  </Button>
+);
 export default function Home() {
   return (
     <div className="container">
@@ -11,7 +27,28 @@ export default function Home() {
       </Head>
 
       <main>
-        <p className="description">Coming soon.</p>
+        <img
+          src="https://i.ibb.co/rw32JGz/HQ2.png"
+          alt="Landboard logo"
+          style={{ width: "70%", maxWidth: 1200 }}
+        />
+        <p className="description" style={{ fontSize: 24 }}>
+          Coming soon.
+        </p>
+        <div style={{}}>
+          <a href="https://twitter.com/landboard_io">
+            <StyledButton
+              text="Twitter"
+              icon={<AiOutlineTwitter style={{ fontSize: "1.2rem" }} />}
+            />
+          </a>
+          <a href="https://t.me/+abXeMctNaR0xNmY0">
+            <StyledButton
+              text="Telegram"
+              icon={<FaTelegram style={{ fontSize: "1.2rem" }} />}
+            />
+          </a>
+        </div>
       </main>
     </div>
   );
